d9e4c050a56c2de7ae7ccc542970ac5be6b99480
Roy Fernandez <u20221b778@upc.edu.pe>
2024-11-10 19:15:33 -0500

    feat(Structure): add colours and dimensity configuration

--- MapOne.py
+++ MapOne.py
@@ -10,3 +10,12 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pac-Man")
 
+# Colores del mapa
+BLACK = (0, 0, 0)  # Fondo del mapa
+WALL_COLOR = (0, 0, 139)  # Color de las paredes
+PELLET_COLOR = (255, 182, 193)  # Color para los pellets
+
+# Configuración de dimensiones del laberinto
+CELL_SIZE = 20
+PELLET_SIZE = 4
+
