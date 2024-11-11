ba452a08a186e76d3bbc746e24fce8d5cb2571cf
Roy Fernandez <u20221b778@upc.edu.pe>
2024-11-10 19:13:47 -0500

    feat(Map): add structure for the windown

---
+++ MapOne.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+# Inicialización de pygame
+pygame.init()
+
+# Configuración del tamaño de la ventana
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pac-Man")
+
